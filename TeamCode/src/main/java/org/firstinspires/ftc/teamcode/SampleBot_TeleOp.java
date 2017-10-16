6846dfb5ee59c7da812bf5ac6b261983d2ab8ede
shirley <32016041+shirley-ye@users.noreply.github.com>
2017-10-16 16:28:13 -0400

    nothing relevant

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleBot_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleBot_TeleOp.java
@@ -51,7 +51,8 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="SampleBot:TeleOp", group="SampleBot")
+@TeleOp(name="Samp " +
+        "leBot:TeleOp", group="SampleBot")
 public class SampleBot_TeleOp extends OpMode{
 
     /* Declare OpMode members. */
