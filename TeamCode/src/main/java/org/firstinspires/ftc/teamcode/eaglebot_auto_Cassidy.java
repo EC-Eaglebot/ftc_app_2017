6a3cf91a6a382fc4b830494162bb98c8bdc9638c
clangenfeld <clangenfeld23@gmail.com>
2017-10-28 09:30:34 -0400

    cassidy autonomous
    autonomous made to identify symbols

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/eaglebot_auto_Cassidy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/eaglebot_auto_Cassidy.java
@@ -1,99 +1,127 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+
 /**
- * This file illustrates the concept of driving a path based on time.
- * It uses the common Pushbot hardware class to define the drive on the robot.
- * The code is structured as a LinearOpMode
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
  *
- * The code assumes that you do NOT have encoders on the wheels,
- *   otherwise you would use: PushbotAutoDriveByEncoder;
- *
- *
- *  The code is written in a simple form with no optimizations.
- *  However, there are several ways that this type of sequence could be streamlined,
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
  *
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Eaglebot: Center Ball", group="Eaglebot")
+
+@TeleOp(name="Basic: Tele OpMode", group="TestBot")
+//@Disabled
 public class eaglebot_auto_Cassidy extends LinearOpMode {
 
-    /* Declare OpMode members. */
-    HardwareSampleBot     robot   = new HardwareSampleBot();
-    private ElapsedTime     runtime = new ElapsedTime();
-
-
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    VuforiaLocalizer vuforia;
 
     @Override
     public void runOpMode() {
 
-
-        /*
-         * Initialize the drive system variables.
-         * The init() method of the hardware class does all the work here
-         */
-        robot.init(hardwareMap);
-
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Ready to run");    //
+        telemetry.addData("Status", "Initialized");
         telemetry.update();
+
+
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = "ATsODcD/////AAAAAVw2lR...d45oGpdljdOh5LuFB9nDNfckoxb8COxKSFX";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        // When the start button is pressed, the rest of the code will execute.
-        // You want to put the actions under here. At the end of the file are
-        // functions that will allow the robot to perform certain actions.
-        // If you want a new action, talk to Cassidy, Corey, or Mr. Steen
-        // and we will give you a hand.
-        // Right now the code is executing "Eaglebot: Center Ball". You'll want
-        // to make the code between // START and // END whatever you want the
-        // robot to do.
-
-        // START
-
-        robot.stopMoving();
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 8)) {
-            telemetry.addData("Stopped", runtime.seconds());
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            while (runtime.seconds() < 1) {
+                leftDrive.setPower(0.2);
+                rightDrive.setPower(0.2);
+            }
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
+            runtime.reset();
+
+            //pick up cube here
+
+            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+            while (vuMark == RelicRecoveryVuMark.UNKNOWN) {
+                //strafe or something
+            }
+            if (vuMark == RelicRecoveryVuMark.LEFT) {
+                telemetry.addData("Key should be placed left", vuMark);
+            }
+            else if (vuMark == RelicRecoveryVuMark.CENTER) {
+                telemetry.addData("Key should be placed center", vuMark);
+            }
+            else if (vuMark == RelicRecoveryVuMark.RIGHT) {
+                telemetry.addData("Key should be placed right", vuMark);
+            }
+            else {
+                telemetry.addData("VuMark not visible", vuMark);
+            }
             telemetry.update();
         }
-        robot.forward(0.5);
-        runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 2)) {
-            telemetry.addData("Moving Forward", runtime.seconds());
-            telemetry.update();
-        }
-        robot.stop();
-
-        // END
-
-        // Below here is where the robot will stop and go to sleep. Make
-        // sure you have the last line above as "robot.stop();" !
-
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
-        sleep(1000);
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
     }
 }
-
-// TOOLBOX OF FUNCTIONS
-// *********************
-/*
-    Totally end the robot's function:       robot.stop();
-    Make the motors stop moving:            robot.stopMoving();
-    Make the robot move forward:            robot.forward(<insert speed here>);
-    Display a message to the screen:        telemetry.addData("Message","value");
-    Update the screen for the message:      telemetry.update();
-    Insert this after a robot.function:     runtime.reset();
-    Determine how long the code has run:    runtime.seconds();
-
-
-*/
-
-
